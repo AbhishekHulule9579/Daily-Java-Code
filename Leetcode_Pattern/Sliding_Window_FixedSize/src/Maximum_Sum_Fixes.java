929c7d532470b0ff4ac0da79b988fab56ef66b3a
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-08-29 19:32:53 +0530

    Initial Commit

--- Leetcode_Pattern/Sliding_Window_FixedSize/src/Maximum_Sum_Fixes.java
+++ Leetcode_Pattern/Sliding_Window_FixedSize/src/Maximum_Sum_Fixes.java
@@ -1,5 +1,22 @@
 public class Maximum_Sum_Fixes {
+    public static int maxSum(int[] arr, int k) {
+        if (arr.length < k) return -1;
+        int windowSum = 0, maxSum = 0;
+
+        for (int i = 0; i < k; i++) windowSum += arr[i];
+        maxSum = windowSum;
+
+        for (int i = k; i < arr.length; i++) {
+            windowSum += arr[i] - arr[i - k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+        return maxSum;
+    }
+
     public static void main(String[] args) {
-        
+        int[] arr = {2, 1, 5, 1, 3, 2};
+        int k = 3;
+        System.out.println("Maximum sum of subarray of size " + k + ": " + maxSum(arr, k));
+
     }
 }
