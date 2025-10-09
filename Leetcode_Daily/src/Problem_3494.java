9e34adcdc5f40b7f30a52b9a1c3c5732ad498098
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-10-09 20:52:40 +0530

    9 Octomber

--- Leetcode_Daily/src/Problem_3494.java
+++ Leetcode_Daily/src/Problem_3494.java
@@ -6,12 +6,9 @@ public class Problem_3494 {
         public long minTime(int[] skill, int[] mana) {
             int[] users = skill;
             int[] products = mana;
-
             int m = products.length;
             int n = users.length;
-
             long[] times = new long[n];
-
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
                     long t = (long) products[i] * users[j];
@@ -22,16 +19,13 @@ public class Problem_3494 {
                         times[j] = Math.max(times[j], times[j - 1]) + t;
                     }
                 }
-
                 for (int j = n - 2; j >= 0; j--) {
                     times[j] = times[j + 1] - (long) products[i] * users[j + 1];
                 }
             }
-
             return times[n - 1];
         }
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -42,7 +36,6 @@ public class Problem_3494 {
         for (int i = 0; i < n; i++) {
             skill[i] = sc.nextInt();
         }
-
         System.out.println("Enter number of elements in mana array:");
         int m = sc.nextInt();
         int[] mana = new int[m];
@@ -50,7 +43,6 @@ public class Problem_3494 {
         for (int i = 0; i < m; i++) {
             mana[i] = sc.nextInt();
         }
-
         Solution solver = new Solution();
         long result = solver.minTime(skill, mana);
         System.out.println("Minimum total time: " + result);
